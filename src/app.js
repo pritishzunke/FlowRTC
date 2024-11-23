dc29379f17217f66f94e21ab99d7c3338b57b786
Pritish <pritish@Pritishs-MacBook-Air.local>
2024-11-23 11:59:09 +0530

    initial HTTP and WS server setup code

--- src/app.js
+++ src/app.js
@@ -1,1 +1,22 @@
+const express = require('express');
+const { createServer } = require('http');
+const env = require('dotenv').config();
+const { Server } = require('socket.io');
 
+const app = express();
+const httpServer = createServer(app);
+const io = new Server(httpServer, {
+    /* options */
+});
+
+//handle connection
+io.on('connection', (socket) => {
+    console.log('New connection:', socket.id);
+    //handle disconnection
+    socket.on('disconnect', () => {
+        console.log('Connection disconnected:', socket.id);
+    });
+});
+
+console.log('Server listening on port ', process.env.PORT);
+httpServer.listen(process.env.PORT);
