e75900b0e7edcb36e36b5de6a4f3f6981fd51ac0
Pritish <pritish@Pritishs-MacBook-Air.local>
2024-11-23 10:17:43 +0530

    configure prettier with eslint and install required packages

---
+++ eslint.config.mjs
@@ -0,0 +1,11 @@
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import eslintConfigPrettier from 'eslint-config-prettier';
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+    eslintConfigPrettier,
+    { files: ['**/*.js'], languageOptions: { sourceType: 'commonjs' } },
+    { languageOptions: { globals: globals.browser } },
+    pluginJs.configs.recommended,
+];
